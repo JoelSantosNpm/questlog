2b13d0ba184ccbfc2f1d73904bf4cb4d7e21e968
JoelSantosNpm <sr.joel.93@gmail.com>
2026-02-12 19:16:43 +0100

    creada y aplicada la primera migración de BBDD

---
+++ prisma/migrations/20260212181320_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Campaign" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Campaign_pkey" PRIMARY KEY ("id")
+);
