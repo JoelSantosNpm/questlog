21108ef644dd9ed822f05fbaa8144f99377dbaa5
JoelSantosNpm <sr.joel.93@gmail.com>
2026-02-18 17:31:18 +0100

    refactor: update import path for Portal component and clean up unused code in Home component

--- src/app/components/portal/portal-card.tsx
+++ src/app/components/portal/portal-card.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { Portal } from "./Portal";
+import { Portal } from "./portal";
 
 interface PortalCardProps {
   index: number;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { PortalCarousel } from "./components/portal/portal-carousel";
 export default function Home() {
   return (
     <main className="relative flex h-screen w-full items-center justify-center overflow-hidden bg-zinc-950">
-      {/* <Portal mode="existing" campaignName="Mi Campaña" />
-      <Portal mode="new" campaignName="Mi Campaña" size={"lg"} /> */}
       <PortalCarousel />
     </main>
   );
