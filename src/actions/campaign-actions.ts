9cf0e86fffaf92e8b8b9e4723c85f67dab6526d9
JoelSantosNpm <sr.joel.93@gmail.com>
2026-02-24 12:21:04 +0100

    feat: enhance createCampaign function with user authentication and error handling

--- src/actions/campaign-actions.ts
+++ src/actions/campaign-actions.ts
@@ -1,20 +1,32 @@
-"use server";
+'use server'
 
-import prisma from "@/lib/prisma";
-import { revalidatePath } from "next/cache";
+import prisma from '@/lib/prisma'
+import { revalidatePath } from 'next/cache'
+import { auth } from '@clerk/nextjs'
 
 export async function createCampaign(formData: FormData) {
-  const name = formData.get("name") as string;
+  const { userId } = auth()
+
+  if (!userId) {
+    throw new Error('Unauthorized')
+  }
+
+  const name = formData.get('name') as string
 
   if (!name) {
-    throw new Error("Name is required");
+    throw new Error('Name is required')
   }
 
   await prisma.campaign.create({
     data: {
       name,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
     },
-  });
+  })
 
-  revalidatePath("/");
+  revalidatePath('/')
 }
