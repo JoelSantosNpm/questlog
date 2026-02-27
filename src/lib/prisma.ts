2eaf14748b1c411209974be8cb578c78f563f72c
JoelSantosNpm <sr.joel.93@gmail.com>
2026-02-27 14:34:07 +0100

    feat: simplificación y eliminación de herramientas innecesarias de pooling en la creación del objeto de Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const connectionString = `${process.env.DATABASE_URL_REMOTE}`
 
 const prismaClientSingleton = () => {
-  const pool = new Pool({
-    connectionString,
-  })
-  const adapter = new PrismaPg(pool)
+  const adapter = new PrismaPg({ connectionString })
   return new PrismaClient({ adapter })
 }
 
