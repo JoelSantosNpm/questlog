526437df8e170312c4fc3060cf89bd672b3c5938
JoelSantosNpm <sr.joel.93@gmail.com>
2026-03-05 18:45:00 +0100

    feat: pass campaigns data to PortalCarousel component in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { PortalCarousel } from './components/portal/portal-carousel'
+import { PortalCarousel } from '@/app/components/portal/portal-carousel'
+import { campaigns } from '@/app/data/mock-campaigns'
 
 export default function Home() {
   return (
-    <div className='relative flex h-full w-full items-center justify-center overflow-hidden'>
-      <PortalCarousel />
+    <div className='relative flex h-full w-full flex-col items-center justify-center overflow-hidden'>
+      <PortalCarousel campaigns={campaigns} />
     </div>
   )
 }
